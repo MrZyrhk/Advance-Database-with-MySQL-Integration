cec520d4de48496a0e6ac657bac50ef07368b7c2
MrZyrhk <93850221+MrZyrhk@users.noreply.github.com>
2025-01-14 15:52:15 +0800

    Add files via upload

---
+++ Advanced_Database_and_Integration_project/src/Advance_Database/LoggerUtil.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Advance_Database;
+
+/**
+ *
+ * @author ambet
+ */
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerUtil {
+
+    private static final Logger logger = Logger.getLogger("MedicalRecordSystem");
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("errors.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+            Logger logger = LoggerUtil.getLogger();
+            logger.severe("Error: Unable to add patient to the database. " + e.getMessage());
+
+            System.err.println("Error setting up logger: " + e.getMessage());
+        }
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
